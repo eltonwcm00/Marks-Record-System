b97ec1178c855fa3a7c844deb03b5d4678c8253c
eltonwcm00 <eltonwcm00@gmail.com>
2021-03-02 15:19:05 +0800

    added CLion configuration

---
+++ cmake-build-debug/CMakeFiles/DCP5301___DSA_FInal_Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DCP5301___DSA_FInal_Project.dir/Assignment.cpp.obj"
+  "DCP5301___DSA_FInal_Project.exe"
+  "DCP5301___DSA_FInal_Project.exe.manifest"
+  "DCP5301___DSA_FInal_Project.pdb"
+  "libDCP5301___DSA_FInal_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DCP5301___DSA_FInal_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
