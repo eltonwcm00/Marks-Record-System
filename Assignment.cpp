68252449a813b77f08dbfc8079b6bef9c4f62811
eltonwcm00 <eltonwcm00@gmail.com>
2021-03-02 15:38:17 +0800

    nicer code indentation

--- Assignment.cpp
+++ Assignment.cpp
@@ -3,134 +3,146 @@ using namespace std;
 
 const int SIZE = 10;
 
-class StudentMarks
-{
- private:
-    struct Student
-        {
+class StudentMarks {
+
+    private:
+
+        struct Student {
+
             string name;
             int id, chem, math, phy;
         };
    
-    Student student[SIZE];
-    int head,tail;
+        Student student[SIZE];
 
- public:
+    public:
 
-    StudentMarks(){}
+        StudentMarks() {}
 
-    void deleteStudent()
-    {
-        Student temp[SIZE];
-        int found=0, ID;
-        char choice;
+        void deleteStudent() {
 
-        /*          Testing          */                   
-        for(int i=0; i<2; i++){
-        cout << "Student " << i+1 << " ID: ";
-        cin >> student[i].id;
-        cout << "Student " << i+1 << " name: ";
-        cin >> student[i].name;
-         cout << "chemistry: ";
-        cin >> student[i].chem;
-         cout << "Phy: ";
-        cin >> student[i].phy;
-         cout << "math: " ;
-        cin >> student[i].math;
-        }
-        /*          End Test        */
+            Student temp[SIZE];
+            int found=0, ID;
+            char choice;
+//
+//            /*          Testing          */
+//            for(int i=0; i<2; i++){
+//                cout << "Student " << i+1 << " ID: ";
+//                cin >> student[i].id;
+//                cout << "Student " << i+1 << " name: ";
+//                cin >> student[i].name;
+//                cout << "chemistry: ";
+//                cin >> student[i].chem;
+//                cout << "Phy: ";
+//                cin >> student[i].phy;
+//                cout << "math: " ;
+//                cin >> student[i].math;
+//           }
+//            /*          End Test        */
             cout << "Please Enter Student ID to delete: ";
             cin >> ID;
+
             found = linearSearch(ID);
 
-            if(found != -1){
-            cout << "Target found"<< endl;
-            cout << "\nStudent ID: " << student[found].id;
-            cout << "\nStudent name: " << student[found].name;
-            cout << "\nSubjects mark: ";
-            cout << "\n\t1.Chemistry: " << student[found].chem;
-            cout << "\n\t1.Physics: " << student[found].phy;
-            cout << "\n\t1.Mathematics: " << student[found].math;
+            if(found != -1) {
 
-            cout << "\n\nAre you sure want to delete? [Y/N] ";
-            cin >> choice;
+                cout << "Target found"<< endl;
+                cout << "\nStudent ID: " << student[found].id;
+                cout << "\nStudent name: " << student[found].name;
+                cout << "\nSubjects mark: ";
+                cout << "\n\t1.Chemistry: " << student[found].chem;
+                cout << "\n\t1.Physics: " << student[found].phy;
+                cout << "\n\t1.Mathematics: " << student[found].math;
 
-            if(choice == 'Y' || choice == 'y'){
-                for(int i=0; i<SIZE; i++){
+                cout << "\n\nAre you sure want to delete? [Y/N] ";
+                cin >> choice;
+
+                if(choice == 'Y' || choice == 'y') {
+
+                    for(int i=0; i<SIZE; i++) {
+
                         temp[i] = student[i];
-                }
+                    }
 
-                for(int i=0; i<SIZE; i++){
-                    if(temp[i].id != ID)
-                        student[i]=temp[i];
+                    for(int i=0; i<SIZE; i++) {
+
+                        if(temp[i].id != ID) {
+                            student[i]=temp[i];
+                        }
+                    }
+                    //student[found].id=-1; student[found].name = " "; student[found].chem=-1; student[found].phy=-1; student[found].math=-1;
                 }
-                //student[found].id=-1; student[found].name = " "; student[found].chem=-1; student[found].phy=-1; student[found].math=-1;
-            }
             
-            }else{
+            } else {
+
                 cout << "StudentID is not found" ;
             }
     }
 
 
-    void search()
-    {
-        int value, found=0;
-        char choice;
+        void search() {
 
-        /*          Testing          */                   
-        for(int i=0; i<2; i++){
-        cout << "Student " << i+1 << " ID: ";
-        cin >> student[i].id;
-        cout << "Student " << i+1 << " name: ";
-        cin >> student[i].name;
-         cout << "chemistry: ";
-        cin >> student[i].chem;
-         cout << "Phy: ";
-        cin >> student[i].phy;
-         cout << "math: " ;
-        cin >> student[i].math;
+            int value, found = 0;
+            char choice;
+//
+//            /*          Testing          */
+//            for(int i=0; i<2; i++){
+//                cout << "Student " << i+1 << " ID: ";
+//                cin >> student[i].id;
+//                cout << "Student " << i+1 << " name: ";
+//                cin >> student[i].name;
+//                cout << "chemistry: ";
+//                cin >> student[i].chem;
+//                cout << "Phy: ";
+//                cin >> student[i].phy;
+//                cout << "math: " ;
+//                cin >> student[i].math;
+//            }
+//            /*          End Test        */
+            do {
+
+                cout << "Please Enter Student ID to search: ";
+                cin >> value;
+                found = linearSearch(value);
+
+                if(found != -1) {
+
+                    cout << "Target found"<< endl;
+
+                    cout << "\nStudent ID: " << student[found].id;
+                    cout << "\nStudent name: " << student[found].name;
+                    cout << "\nSubjects mark: ";
+                    cout << "\n\t1.Chemistry: " << student[found].chem;
+                    cout << "\n\t1.Physics: " << student[found].phy;
+                    cout << "\n\t1.Mathematics: " << student[found].math;
+
+                } else {
+
+                    cout << "StudentID is not found" ;
+                }
+
+                cout << "Search again?[Y/N] ";
+                cin >> choice;
+
+            } while(choice != 'Y' || choice != 'y');
+
         }
-        /*          End Test        */
-        do{
-            cout << "Please Enter Student ID to search: ";
-            cin >> value;
-            found = linearSearch(value);
 
-            if(found != -1){
-            cout << "Target found"<< endl;
+        int linearSearch(int value) {
 
-            cout << "\nStudent ID: " << student[found].id;
-            cout << "\nStudent name: " << student[found].name;
-            cout << "\nSubjects mark: ";
-            cout << "\n\t1.Chemistry: " << student[found].chem;
-            cout << "\n\t1.Physics: " << student[found].phy;
-            cout << "\n\t1.Mathematics: " << student[found].math;
+            for (int i=0; i<=SIZE; i++) {
 
-            }else{
-                cout << "StudentID is not found" ;
+                if (student[i].id == value)
+                    return i;
             }
 
-            cout << "Search again?[Y/N] ";
-            cin >> choice;
-        }while(choice != 'Y' || choice != 'y');
-        
-    }
-
-    int linearSearch(int value)
-    {
-        for (int i=0; i<=SIZE; i++)
-        {
-            if (student[i].id == value)
-                return i;
+            return -1;
         }
-        return -1;	
-    }
 
 };
 
-int main()
-{
+int main() {
+
     //Student s[SIZE];
     StudentMarks sm;
     string name;
@@ -138,8 +150,6 @@ int main()
     char grade;
 
     sm.deleteStudent();
-
-    
 
     return 0;
 }
